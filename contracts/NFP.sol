0da82c0dc4be4eed4827e2f37f2c97e8fcc5e38e
KowalewskiPawel <pawelkowalewsk@gmail.com>
2022-09-03 00:57:03 +0200

    modifier for NFT owner added

--- contracts/NFP.sol
+++ contracts/NFP.sol
@@ -4,18 +4,27 @@ pragma solidity ^0.8.0;
 
 import {DataTypes} from "./DataTypes.sol";
 
-contract NFP {
+interface ERC721Interface {
+    function ownerOf(uint256 _tokenId) external view returns (address);
+}
 
+contract NFP {
     mapping(string => DataTypes.NFTInfo) public nftsRegistered;
 
     constructor() {
     }
 
-    function printNFT(string memory _nfcTag, address _nftAddress, uint256 _nftId)
-        public
+    modifier isOwnerOfNft(address _nftAddress, uint256 _nftId) {
+        ERC721Interface collectionToCheck = ERC721Interface(_nftAddress);
+        require(collectionToCheck.ownerOf(_nftId) == msg.sender, "You are not the owner of the NFT");
+        _;
+    }
+
+    function printNFT(string memory _nfcTag, address _nftAddres, uint256 _nftId)
+        public isOwnerOfNft(_nftAddres, _nftId)
     {
         DataTypes.NFTInfo memory newNFT = DataTypes.NFTInfo({
-            nftAddres: _nftAddress,
+            nftAddres: _nftAddres,
             nftId: _nftId,
             isActive: false
         });
